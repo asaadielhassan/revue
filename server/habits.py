7968dd3a800d08d8e36ef0e84c4a77c698be9627
Drew Friend <dafriend@knox.edu>
2021-04-16 11:57:26 -0500

    Added Month Dict

--- server/habits.py
+++ server/habits.py
@@ -10,6 +10,20 @@ class habits:
     curr_Date = [timer.strftime("%m"), timer.strftime("%d")]
     end_Date = []
     complete = 0
+    monthDict = {
+        1 : 31,
+        2 : 28,
+        3 : 31,
+        4 : 30,
+        5 : 31,
+        6 : 30,
+        7 : 31,
+        8 : 31,
+        9 : 30,
+        10 : 31,
+        11 : 30,
+        12 : 31
+    }
 
     def habits(self, name, days, end_Date):
         self.name = name
