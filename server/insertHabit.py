5d2f73484691a37ecc10a209503b83bf773ad9df
Drew Friend <dafriend@knox.edu>
2021-04-17 14:57:47 -0500

    Fixed bug where equals was used instead of :

--- server/insertHabit.py
+++ server/insertHabit.py
@@ -12,12 +12,12 @@ def connectDB():
 
 def insertHabit(user, habit):
     entry = {
-        "user" = user,
-        "name" = habit.getName(),
-        "days" = habit.getDays(),
-        "start_Day" = habit.getStartDay(),
-        "start_Date" = habit.getStartDate(),
-        "end_Date" = habit.getEndDate()
+        "user":user,
+        "name":habit.getName(),
+        "days":habit.getDays(),
+        "start_Day":habit.getStartDay(),
+        "start_Date":habit.getStartDate(),
+        "end_Date":habit.getEndDate()
     }
 
     db.habits.insert_one(entry)
