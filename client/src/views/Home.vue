e357c880d16258be7da120daf27bc3c336beafc9
therealthaps <abthapa@knox.edu>
2021-04-26 19:50:50 -0500

    Changed the home page- added background and styled the page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,29 +1,65 @@
-<template lang="html">
-  <div class="home container">
-    <div class = "welcome">
+ <template lang="html">
+  <!-- <div class="home container"> -->
+    <!-- <div class = "welcome">
       <h1 v-if="!$store.state.isUserLoggedIn"> Welcome to Productify </h1>
       <p v-if="!$store.state.isUserLoggedIn"> Add tasks, create beautiful grids, and see your productivity come to life with Productify! Make your grids public and share them with the world! Check out the grid feed to see what users have been creating. Start by creating an account here:</p>
       <router-link v-if="!$store.state.isUserLoggedIn" class="button" :to="{ name: 'SignUp', params: {} }">Sign Up</router-link>
-    </div>
-    <h1 v-if="$store.state.isUserLoggedIn">Hello, {{ $store.state.user.username }}</h1>
+    </div> -->
+    <!-- <h1 v-if="$store.state.isUserLoggedIn">Hello, {{ $store.state.user.username }}</h1> -->
     <!-- <PostPreview v-for="post in posts" :key="post.id" :post="post">
       {{ post.title }}
     </PostPreview> -->
 
-    <CreateButton></CreateButton>
+    <!-- <CreateButton></CreateButton> -->
+  <!-- </div> -->
+  <div class = "container">
+    <div class = "content">
+      <!-- before logging in  -->
+      <h1  v-if="!$store.state.isUserLoggedIn"> Welcome to Productify </h1>
+
+      <!-- after loggin in  -->
+      <h1 v-if="$store.state.isUserLoggedIn">Hello, {{ $store.state.user.username }}</h1>
+
+      <!-- description of productivity  -->
+      <p> 
+        Add tasks, create beautiful grids, and see your productivity come to life with Productify!<br>
+        Make your grids public and share them with the world! <br>
+        Check out the grid feed to see what users have been creating. <br> <br>
+      </p>
+
+      <!-- create an account button  -->
+      <p v-if="!$store.state.isUserLoggedIn" >
+        Start by creating an account here:
+      </p>
+      <router-link v-if="!$store.state.isUserLoggedIn" class = "btn" :to="{ name: 'SignUp', params: {} }">Create New Account</router-link>
+      
+      <p v-if="!$store.state.isUserLoggedIn" >
+        Already have an account?
+      </p>
+      <router-link v-if="!$store.state.isUserLoggedIn" class = "btn" :to="{ name: 'Login', params: {} }">Log in</router-link>
+
+      <!-- to do image before logging in  -->
+      <img v-if="!$store.state.isUserLoggedIn" src = "../assets/images/todo.png" class = "feature-img">
+      <!-- TODO: add a random image after logging in  -->
+
+      <router-link v-if="$store.state.isUserLoggedIn" class = "btn" :to="{ name: 'CreatePost'}">Create A New Grid</router-link>
+      <!-- <br> -->
+      <router-link v-if="$store.state.isUserLoggedIn" class = "btn2" :to="{ name: 'User', params: { username: $store.state.user.username } }">View My Current Grids</router-link>  
+    </div> 
   </div>
+
 </template>
 
 <script>
 // import PostPreview from '@/components/PostPreview'
 import PostsService from '@/services/PostsService'
-import CreateButton from '@/components/CreateButton'
+// import CreateButton from '@/components/CreateButton'
 
 export default {
   name: 'home',
 
   // components: { PostPreview, CreateButton },
-  components: {CreateButton},
+  // components: {CreateButton},
 
   data() {
     return {
@@ -40,25 +76,110 @@ export default {
 }
 </script>
 
-<style scoped lang="css">
-  .welcome {
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .container {
+    height: 100vh;
+    width: 100%;
+    background-image: url('../assets/images/custom.png');
+    background-position: center;
+    background-size: cover;
+    padding-left: 5%;
+    padding-right: 5%;
+    box-sizing: border-box;
+    position: relative;
+  }
+
+  .content h1 {
+    font-size: 60px;
+    font-weight: 100;
+    margin-top: 24px;
+    margin-bottom: 15px;
+    color: #2D3142;
+
+  }
+
+  .content p {
+    font-size: 20px;
+    color: #4F5D75;
+    line-height: 1.3;
+    
+  }
+
+  .content{
+    margin-left: 5%;
+    margin-top: 18%;
+  }
+
+  .content .btn{
+    display: inline-block;
+    background: linear-gradient(45deg, #eb9766, #4F5D75);
+    border-radius: 6px;
+    padding: 15px 25px;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: #fff;
+    box-shadow: 3px 8px 22px rgba(94, 28, 68, 0.15);
+    margin-top: 5px;
+    margin-bottom: 30px;
+  }
+
+  .content .btn:hover{
+    background: linear-gradient(45deg, #a76b48, #333b4b);
+    transition: all 0.2s ease-in-out;
+  }
+
+  .content .btn2{
+    display: inline-block;
+    background: linear-gradient(45deg, #eb9766, #4F5D75);
+    border-radius: 6px;
+    padding: 15px 25px;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: #fff;
+    box-shadow: 3px 8px 22px rgba(94, 28, 68, 0.15);
+    margin-top: 40px;
+    margin-left: 5%;
+  }
+
+  .content .btn2:hover{
+    background: linear-gradient(45deg, #a76b48, #333b4b);
+    transition: all 0.2s ease-in-out;
+  }
+
+  .feature-img {
+    height: 90%;
+    position: absolute;
+    bottom: 0;
+    right: 200px;
+  }
+
+  
+  
+
+
+  /* .welcome {
     margin-top: 60px;
     font-size: 1.7em;
     text-align: center;
-  }
+  } */
  
-  p{
-    font-size: 1.6em;
+  /* p{
+    font-size: 1.0em;
     margin: 65px;
     margin-bottom: 100px;
-  }
+  } */
 
-  .button{
-    padding: 30px;
-    font-size: 1.5em;
-    border: double 30px white;
-    margin-top: 80px;
-  }
+  /* .button{ */
+    /* padding: 10px; */
+    /* font-size: 1.0em; */
+    /* border: double 30px white; */
+    /* margin-top: 80px; */
+  /* } */
 
 
 </style>
