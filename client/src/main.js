fee059684cb8e167183ce1d26139b7d03355a71b
therealthaps <abthapa@knox.edu>
2021-05-26 17:14:41 -0500

    Redid and restyle MyGrids page and the create a new grid form

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueApexCharts from "vue3-apexcharts";
+
+const app = createApp(App);
+app.use(VueApexCharts);
 
 require('@/assets/style.css')
 
